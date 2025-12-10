4b23063bd8f66a154385c79d9631f4bab3f37097
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-12-10 15:30:31 -0500

    FHIR resources for study population

---
+++ cumulus_library_glioma/athena/glioma__cohort_study_population_dx.sql
@@ -0,0 +1,14 @@
+create table glioma__cohort_study_population_dx as
+select distinct
+    DX.category_code    as dx_category_code,
+    DX.code             as dx_code,
+    replace(DX.code_display, chr(10), ' ') as dx_display,
+    DX.system           as dx_system,
+    DX.condition_ref    as condition_ref,
+    study_population.*
+from
+    glioma__cohort_study_population as study_population,
+    core__condition as DX
+where
+    study_population.encounter_ref = DX.encounter_ref
+;
---
+++ cumulus_library_glioma/athena/glioma__cohort_study_population_rx.sql
@@ -0,0 +1,17 @@
+create table glioma__cohort_study_population_rx as
+select distinct
+    RX.status               as rx_status,
+    RX.category_code        as rx_category_code,
+    RX.category_system      as rx_category_system,
+    RX.category_display     as rx_category_display,
+    RX.medication_code      as rx_code,
+    lower(replace(RX.medication_display, chr(10), ' ')) as rx_display,
+    RX.medication_system    as rx_system,
+    RX.medicationrequest_ref as medicationrequest_ref,
+    study_population.*
+from
+    glioma__cohort_study_population as study_population,
+    core__medicationrequest as RX
+where
+    study_population.encounter_ref = RX.encounter_ref
+;
