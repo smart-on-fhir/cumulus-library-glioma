630247ce87b2c886cb87aab7002e6b8e804a50ef
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-01-20 14:45:49 -0800

    added case definition "pre, peri, post" to derived cohorts

--- cumulus_library_glioma/athena/glioma__cohort_casedef_lab.sql
+++ cumulus_library_glioma/athena/glioma__cohort_casedef_lab.sql
@@ -2,6 +2,10 @@ create  table glioma__cohort_casedef_lab as
 select  distinct
         casedef.days_since,
         casedef.ordinal_since,
+        casedef.pre,
+        casedef.peri,
+        casedef.peri_post,
+        casedef.post,
         casedef.dx_category_code,
         casedef.dx_system,
         casedef.dx_code,
