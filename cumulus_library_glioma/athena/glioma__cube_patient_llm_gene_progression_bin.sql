8a9404c2df58f7d6c83efe471dd1f53370788b71
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-02-12 13:46:52 -0800

    + CUBE gene progression bin (discrete outcome)

---
+++ cumulus_library_glioma/athena/glioma__cube_patient_llm_gene_progression_bin.sql
@@ -0,0 +1,83 @@
+CREATE TABLE glioma__cube_patient_llm_gene_progression_bin AS (
+    WITH
+    null_replacement AS (
+        SELECT
+            subject_ref,
+            coalesce(
+                cast(braf_altered AS varchar),
+                'cumulus__none'
+            ) AS braf_altered,
+            coalesce(
+                cast(braf_fusion AS varchar),
+                'cumulus__none'
+            ) AS braf_fusion,
+            coalesce(
+                cast(braf_v600e AS varchar),
+                'cumulus__none'
+            ) AS braf_v600e,
+            coalesce(
+                cast(idh_mutant AS varchar),
+                'cumulus__none'
+            ) AS idh_mutant,
+            coalesce(
+                cast(progression_bin AS varchar),
+                'cumulus__none'
+            ) AS progression_bin,
+            coalesce(
+                cast(symptom_burden_bin AS varchar),
+                'cumulus__none'
+            ) AS symptom_burden_bin,
+            coalesce(
+                cast(visual_status_bin AS varchar),
+                'cumulus__none'
+            ) AS visual_status_bin
+        FROM glioma__llm_gene_progression
+        
+    ),
+
+    powerset AS (
+        SELECT
+            count(DISTINCT subject_ref) AS cnt_subject_ref,
+            "braf_altered",
+            "braf_fusion",
+            "braf_v600e",
+            "idh_mutant",
+            "progression_bin",
+            "symptom_burden_bin",
+            "visual_status_bin",
+            concat_ws(
+                '-',
+                COALESCE("braf_altered",''),
+                COALESCE("braf_fusion",''),
+                COALESCE("braf_v600e",''),
+                COALESCE("idh_mutant",''),
+                COALESCE("progression_bin",''),
+                COALESCE("symptom_burden_bin",''),
+                COALESCE("visual_status_bin",'')
+            ) AS id
+        FROM null_replacement
+        GROUP BY
+            cube(
+            "braf_altered",
+            "braf_fusion",
+            "braf_v600e",
+            "idh_mutant",
+            "progression_bin",
+            "symptom_burden_bin",
+            "visual_status_bin"
+            )
+    )
+
+    SELECT
+        p.cnt_subject_ref AS cnt,
+            p."braf_altered",
+            p."braf_fusion",
+            p."braf_v600e",
+            p."idh_mutant",
+            p."progression_bin",
+            p."symptom_burden_bin",
+            p."visual_status_bin"
+    FROM powerset AS p
+    WHERE 
+        p.cnt_subject_ref >= 10
+);
